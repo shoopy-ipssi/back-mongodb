1b971419aee3f73782690fcd4bab26e8a816ed9a
Damien Bouyeure <damien.bouyeure@gmail.com>
2020-05-01 17:58:36 +0200

    add parent and children connect

--- app/routes/project_routes.js
+++ app/routes/project_routes.js
@@ -38,6 +38,31 @@ module.exports = function (app, db) {
             }
         })
     })
+    app.patch('/scenario/connect', (req, res) => {
+        let parentChildren = "";
+        parentChildren = req.body;
+        console.log(parentChildren)
+
+
+        const detailParent = {'_id': new ObjectId(parentChildren.idParent)};
+        const detailChildren = {'_id': new ObjectId(parentChildren.idChildren)};
+
+
+        db.collection('scenario').updateOne(detailParent, {$push: {"childrens": new ObjectId(parentChildren.idChildren)}}, (err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occured'});
+            }
+        })
+        db.collection('scenario').updateOne(detailChildren, {$push: {"parents": new ObjectId(parentChildren.idParent)}}, (err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occured'});
+            } else {
+                res.send("Parents and Children good")
+            }
+        })
+
+
+    })
 
 
 }
