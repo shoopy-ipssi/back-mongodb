5733c9496cbcfcc91358bf5a96241606dbf0615b
Damien Bouyeure <damien.bouyeure@gmail.com>
2020-07-01 16:11:11 +0200

    update scenario/:id

--- app/routes/project_routes.js
+++ app/routes/project_routes.js
@@ -1,43 +1,43 @@
 let ObjectId = require('mongodb').ObjectID
 const bodyParser = require('body-parser')
 module.exports = function (app, db) {
-    app.use(bodyParser.json())
-    //
-    app.get('/scenario/:id', (req, res) => {
-        const id = req.params.id
-        const details = {'_id': new ObjectId(id)};
-        db.collection('scenario').findOne(details, (err, item) => {
-            if (err) {
-                res.send({'error': 'An error has occured'});
-            } else {
-                res.send(item)
-            }
-        })
+  app.use(bodyParser.json())
+  //
+  app.get('/scenario/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10)
+    const details = {'scenario_id': id};
+    db.collection('scenario').find(details).toArray((err, item) => {
+      if (err) {
+        res.send({'error': 'An error has occured'});
+      } else {
+        res.send(item)
+      }
     })
-    app.get('/scenario', (req, res) => {
-        db.collection('scenario').find().toArray((err, item) => {
-            if (err) {
-                res.send({'error': 'An error has occured'});
-            } else {
-                res.send(item)
-            }
-        })
+  })
+  app.get('/scenario', (req, res) => {
+    db.collection('scenario').find().toArray((err, item) => {
+      if (err) {
+        res.send({'error': 'An error has occured'});
+      } else {
+        res.send(item)
+      }
     })
-    app.post('/scenario/add', (req, res) => {
+  })
+  app.post('/scenario/add', (req, res) => {
 
 
-        let newScenar = "";
-        newScenar = req.body;
-        console.log(newScenar)
+    let newScenar = "";
+    newScenar = req.body;
+    console.log(newScenar)
 
-        db.collection('scenario').insertOne(newScenar, (err, item) => {
-            if (err) {
-                res.send({'error': 'An error has occured'});
-            } else {
-                res.send(item.insertedId)
-            }
-        })
+    db.collection('scenario').insertOne(newScenar, (err, item) => {
+      if (err) {
+        res.send({'error': 'An error has occured'});
+      } else {
+        res.send(item.insertedId)
+      }
     })
+  })
 
 
 }
